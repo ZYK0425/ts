e85edeeefaadcbad3673c9de913ea66faa0f3bd3
赵英奎 <zhaoyingkui@1.lan>
2015-10-23 00:24:57 +0800

    1

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 赵英奎 on 15/10/23.
+//  Copyright (c) 2015年 赵英奎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 赵英奎 on 15/10/23.
+//  Copyright (c) 2015年 赵英奎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
